586ff0bfc1b2eb1ddbba5e9c5408a705bf7b160e
Timothée DURAND <timothee.durand70@gmail.com>
2021-10-07 15:19:47 +0200

    Revert "Merge branch 'master' into bullet-script"
    This reverts commit 7e690210fc1c769f072f79bfae35282594fbdfe2.

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -11,18 +11,18 @@ public class BulletScript : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
+            Debug.Log("HEY");
             TestOtherPerso otherPlayer = collision.gameObject.GetComponent<TestOtherPerso>();
-
+            Debug.Log(collision.gameObject);
             if (otherPlayer)
             {
                 otherPlayer.Health -= damage;
+                Debug.Log(otherPlayer.Health);
                 GameObject particle = Instantiate(particles);
                 particle.GetComponent<Transform>().position = collision.GetContact(0).point;
                 Destroy(particle, 5f);
             }
         }
         Destroy(gameObject);
-
-
     }
 }
