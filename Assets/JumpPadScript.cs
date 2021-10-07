286169ddae09373d6928d679e065473f9e0d048d
Timothée DURAND <timothee.durand70@gmail.com>
2021-10-07 14:54:12 +0200

    jump pod

---
+++ Assets/JumpPadScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadScript : MonoBehaviour
+{
+    public float jumpForce = 1000f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+         
+            Rigidbody rBody = other.gameObject.GetComponent<Rigidbody>();
+            
+            rBody.AddForce(other.gameObject.transform.up * jumpForce);
+        }
+        
+    }
+  
+}
